21d4199abc2a71aa1acbd6efe6a5b926063c2722
Stephen Cleveland <cosmokai2000@gmail.com>
2023-05-05 19:53:44 -0500

    task 4 commit

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -22,12 +22,13 @@ public class Job {
         nextId++;
     }
 
-    public Job (String name, Employer employer, Location location, PositionType positionType) {
+    public Job (String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
         this();
         this.name = name;
         this.employer = employer;
         this.location = location;
         this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
     }
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
